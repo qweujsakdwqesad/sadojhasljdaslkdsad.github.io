05be6d4089f8f571585177e26216658fd10d9248
qweujsakdwqesad <maxeyspector@outlook.com>
2024-12-10 22:08:30 +0000

    asd

--- server.js
+++ server.js
@@ -3,20 +3,32 @@ const app = express();
 
 let jonConfirmed = false;
 
+// Middleware to set CORS headers
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://qweujsakdwqesad.github.io'); // Allow all pages on this domain
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS'); // Allowed HTTP methods
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allowed request headers
+  next();
+});
+
+// API to confirm access
 app.get('/api/confirm', (req, res) => {
   jonConfirmed = true;
   res.send('Jon has confirmed access!');
 });
 
+// API to check access
 app.get('/api/check', (req, res) => {
   res.send(jonConfirmed.toString());
 });
 
+// API to reset access
 app.get('/api/reset', (req, res) => {
   jonConfirmed = false;
   res.send('Jon confirmation has been reset!');
 });
 
+// API to provide server info
 app.get('/api/info', (req, res) => {
   const forwardedFor = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   const forwardedHost = req.headers['x-forwarded-host'] || req.headers.host;
