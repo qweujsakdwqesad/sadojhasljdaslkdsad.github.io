87c89b1e20f8d7b18ce9338639606dc3d310ed40
qweujsakdwqesad <maxeyspector@outlook.com>
2024-12-13 14:38:00 +0000

    asd

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,53 @@
-// vite.config.js
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { shortcutsPlugin } from 'vite-plugin-shortcuts';
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000', // The port where your Node.js server is running
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
+  plugins: [
+    shortcutsPlugin({
+      shortcuts: [
+        {
+          key: 'c',
+          description: 'close console',
+          action: (server) => {
+            console.log('Closing console'); // Debug log
+            server.config.logger.clearScreen('error');
+          },
+        },
+        {
+          key: 's',
+          description: 'reset console',
+          action: (server) => {
+            console.log('Resetting console'); // Debug log
+            server.config.logger.clearScreen('error');
+            server.printUrls();
+          },
+        },
+        {
+          key: 'r',
+          description: 'restart the server',
+          async action(server) {
+            console.log('Restarting server'); // Debug log
+            await server.restart();
+          },
+        },
+        {
+          key: 'u',
+          description: 'show server url',
+          action(server) {
+            console.log('Showing server URL'); // Debug log
+            server.config.logger.info('');
+            server.printUrls();
+          },
+        },
+        {
+          key: 'q',
+          description: 'quit',
+          async action(server) {
+            console.log('Quitting server'); // Debug log
+            await server.close().finally(() => process.exit());
+          },
+        },
+      ],
+    }),
+  ],
 });
